082c6c479819f92c86e0a96451d6d975d43800bb
ahmedbrini <ahmedbrini321@gmail.com>
2023-11-29 22:14:06 +0100

    done

--- js/page3.js
+++ js/page3.js
@@ -7,39 +7,43 @@ function generateID() {
   var id = generateID();
   var idhotel=id()
   
-  function makehotel(idhotel,title,price,star){
+  function makehotel(idhotel,title,price,star,descr){
     var hotel={
       id:idhotel,
       title:title,
       price:price,
       star:star,
+      descr:descr
     }
     return hotel
   }
-  var hotel1=makehotel(new Date(),'El Mouradi',"200 Dt/nuit",'4 Stars')
+  var hotel1=makehotel(new Date(),'El Mouradi',"200 Dt/nuit",'4 Stars',"All inclusive")
   
   
   $("p1").text(hotel1.id)
   $("p2").text(hotel1.title)
   $("p3").text(hotel1.price)
   $("p4").text(hotel1.star)
+  $("p13").text(hotel1.descr)
   
   
-   var hotel2=makehotel(new Date(),"Radisson Palace","250 Dt/nuit","5 Stars")
+   var hotel2=makehotel(new Date(),"Radisson Palace","250 Dt/nuit","5 Stars","Demi pension")
   
   
   $("p5").text(hotel2.id)
   $("p6").text(hotel2.title)
   $("p7").text(hotel2.price)
   $("p8").text(hotel2.star)
+  $("p14").text(hotel2.descr)
   
-  var hotel3=makehotel(new Date(),"Royal Azur","180 Dt/nuit","4 Stars")
+  var hotel3=makehotel(new Date(),"Royal Azur","180 Dt/nuit","4 Stars","All inclusive")
   
   
   $("p9").text(hotel3.id)
   $("p10").text(hotel3.title)
   $("p11").text(hotel3.price)
   $("p12").text(hotel3.star)
+  $("p15").text(hotel3.descr)
   
 
 
